53082f0988ff5ed8f94b9143a8bb7e45d672f31a
vv_tomilin <backatom1@gmail.com>
2021-10-30 11:33:58 +0500

    Внедрил свое приложение в Electron при помощи Electron Forge. В данный момент проблема с блокировкой fetch() запросов из React компонента

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+
+import SelectCurrency from './components/SelectCurrency.jsx';
+
+import './styles/app.scss';
+
+function App() {
+  
+  const [rates, setRates] = useState({});
+  const [currencyNames, setCurrencyNames] = useState({});
+  const [isLoadingRates, setIsLoadingRates] = useState(false);
+  const [isLoadingNames, setIsLoadingNames] = useState(false);
+
+  useEffect(() => {
+    fetch('http://data.fixer.io/api/latest?access_key=7736be7008dd10910ea6e2ac4a8c1dae')
+          .then(response => response.json())
+          .then((json) => {
+            setRates(json.rates);
+            setIsLoadingRates(true);
+          });
+    
+    fetch('http://data.fixer.io/api/symbols?access_key=7736be7008dd10910ea6e2ac4a8c1dae')
+          .then(response => response.json())
+          .then((json) => {
+            setCurrencyNames(json.symbols);
+            setIsLoadingNames(true);
+          })
+  }, []);
+
+  return (
+      <div className="app">
+        <h1 className='app__title'>Exchange rate</h1>
+        <div>
+          {isLoadingRates && isLoadingNames
+            ? <SelectCurrency
+            rates={rates}
+            currencyNames={currencyNames}/>
+            : <p className='app__loading'>Loading...</p>}
+        </div>
+      </div>
+  );
+}
+
+export default App;
